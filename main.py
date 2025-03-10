89c525fbacbb5c5abfa5b778b4c3e577737f9903
rahulchandrawanshiji <161007517+rahulchandrawanshiji@users.noreply.github.com>
2025-03-10 16:18:41 +0530

    update which add a line thet ensure
    there a long code a part of time 8 month thet provide the snnse are exutible and defination about sunstances

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ async def create_item(item: Item):
 '''Create your data mode l¶
 Then you declare your data model as a class that inherits from BaseModel.
 
-Use standard Python types for all the attributes:
+Use standard Python types these file extenderd for which another way these also include functionlaliy ther are 
+substruction unsessary precause devident particle for all the attributes:
 
 
 '''
