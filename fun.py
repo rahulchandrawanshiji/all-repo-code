b9c99aabc07900b419ad5acd1cf8bbfed6f03e61
rahulchandrawanshiji <161007517+rahulchandrawanshiji@users.noreply.github.com>
2025-03-10 17:24:10 +0530

    a single function added  in the file the file name  fun.py
    a changes that we puch the main branch and the last option are the rc branch but they deriven are sucessfully are whch derove and the changes which script more relible and efficent and driven

---
+++ fun.py
@@ -0,0 +1,35 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+def substances(miner,major):
+    if miner<=major:
+        if miner%2==0:
+            print("all the subtitle and the dividnet are driven at the same time by the functin and the variable are avaible at the time :")
+        else:
+            print("ther error are raised because function not pass ")
+    else:
+        print("the function are not working and the particular persentage are not divent by the parsentage ")
+ans = substances()
+# Calling functions one by one
+result1 = add(10, 5)
+print(f"Addition: {result1}")
+
+result2 = subtract(10, 5)
+print(f"Subtraction: {result2}")
+
+result3 = multiply(10, 5)
+print(f"Multiplication: {result3}")
+
+result4 = divide(10, 5)
+print(f"Division: {result4}")
